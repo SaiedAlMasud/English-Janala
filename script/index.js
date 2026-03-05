70c1d04e54a1b051f04f3f282bb41bb794409e23
Ankon 190 <ankon190@gmail.com>
2026-03-05 19:43:32 +0600

    lesson active button functionality added

--- script/index.js
+++ script/index.js
@@ -8,7 +8,15 @@ const loadLevelWord = (id) =>{
     const url = `https://openapi.programming-hero.com/api/level/${id}`;
     fetch(url).
     then(res => res.json()).
-    then(data => displayLevelWord(data.data))
+    then(data => {
+        const allbtn = document.getElementsByClassName("lesson-btn");
+        for(let btn of allbtn){
+            btn.classList.remove("btn-active");
+        }
+        const clickedbtn = document.getElementById(`lesson-${id}`);
+        clickedbtn.classList.add("btn-active");
+        displayLevelWord(data.data);
+    })
 }
 const displayLevelWord = (words) =>{
     //console.log(words);
@@ -31,11 +39,11 @@ const displayLevelWord = (words) =>{
         const wordDiv = document.createElement("div");
         wordDiv.innerHTML = `
             <div class="bg-white w-11/12 mx-auto rounded-xl shadow-sm text-center py-6 px-5 space-y-4">
-                <h2 class="font-bold text-xl">${word.word}</h2>
+                <h2 class="font-bold text-xl">${word.word ? word.word: "শব্দ পাওয়া যায়নি"}</h2>
                 <p class="font-semibold">Meaning / Pronunciation</p>
-                <div class="font-bangla font-medium text-xl text-[18181B]">${word.meaning} / ${word.pronunciation}</div>
+                <div class="font-bangla font-medium text-xl text-[18181B]">${word.meaning ? word.meaning: "অর্থ পাওয়া যায়নি"} / ${word.pronunciation ? word.pronunciation: "উচ্চারণ পাওয়া যায়নি"}</div>
                 <div class="flex justify-between items-center">
-                    <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+                    <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i onclick="my_modal_5.showModal()" class="fa-solid fa-circle-info"></i></button>
                     <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
                 </div>
             </div>
@@ -56,7 +64,7 @@ const displayLessons = (lessons) =>{
         //create a element for each lesson
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
-            <button onclick="loadLevelWord(${lesson.level_no})"class="btn btn-outline btn-primary mx-4">
+            <button id="lesson-${lesson.level_no}" onclick="loadLevelWord(${lesson.level_no})"class="btn btn-outline btn-primary mx-4 lesson-btn">
                 <i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}
             </button>
         `;
